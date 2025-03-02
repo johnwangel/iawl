9bcd8ca22a0b9215b28828224e78712ae1cf9239
johnwangel <ja727@njit.edu>
2025-03-01 19:55:17 -0500

    snowflakes

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -26,6 +26,49 @@ export function Main(props){
 							</div>				 
 
 
+					<div class="snowflakes" aria-hidden="true">
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					  <div class="snowflake">
+					    <div class="inner">❅</div>
+					  </div>
+					</div>
+
+
+
+
+
 				 </div>
 
 
