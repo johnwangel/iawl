1b474646e3378d9fb8d9ce23da58e599f8fefef4
johnwangel <ja727@njit.edu>
2025-04-13 16:53:09 -0400

    new animated header

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -23,19 +23,41 @@ export function Main(props){
 					<Snowflakes />
 
 					<div className='op-container full'>
-							<video className='video-container' controls poster={LOGO}>
-							  <source src='' type="video/mp4" />
-								Your browser does not support the video tag.
-							</video>
-							<div className='authors'>
+							<div className='hero'>
+
 								<div className='au-cont left'>
 									<div className='super-title'> Music By:</div>
 									<div className='author'>Andrew Abrams</div>
 								</div>
+
+								<div className='photo-group'>
+									<img className='img-base' src='./assets/photos/logo_bg.png' />
+									<img className='img-overlay' id='text1' src='./assets/photos/logo_text1.png' />
+									<img className='img-overlay' id='text2' src='./assets/photos/logo_text2.png' />
+									<img className='img-overlay' id='text3' src='./assets/photos/logo_text3.png' />
+									<img className='img-overlay' id='text4' src='./assets/photos/logo_text4.png' />
+									<img className='img-overlay' id='moon1' src='./assets/photos/logo_moon.png' />
+								</div>
+
 								<div className='au-cont right'>
 									<div className='super-title'> Book &amp; Lyrics By:</div>
-									<div className='author'> John Atkins </div>
+									<div className='author'> John W. Atkins </div>
 								</div>
+
+							</div>
+							
+							<div className='authors'>
+
+								<div className='au-cont left'>
+									<div className='super-title'> Music By:</div>
+									<div className='author'>Andrew Abrams</div>
+								</div>
+
+								<div className='au-cont right'>
+									<div className='super-title'> Book &amp; Lyrics By:</div>
+									<div className='author'> John W. Atkins </div>
+								</div>
+
 							</div>
 							<div className='ack'>
 								<i>It's A Wonderful Life</i> is based on the story 'The Greatest Gift' by Philip Van Doren Stern.
@@ -168,7 +190,10 @@ function Videos(props){
 function About(props){
 	return	<div className='about-container'>
 						<div className='description'>
-							Show description goes here.
+							The timeless Christmas classic is brought to life onstage with all the familiar moments you cherish, 
+							and a lot of new surprises along the way.
+
+
 						</div>
 					</div>
 }
